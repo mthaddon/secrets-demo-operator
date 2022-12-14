6d81d14ea3b48842f634960ef5ab64bedec48844
Tom Haddon <tom.haddon@canonical.com>
2022-12-14 10:20:35 +0100

    Add support for the case where we don't support secrets due to older Juju

--- src/charm.py
+++ src/charm.py
@@ -9,6 +9,7 @@ import random
 import string
 
 from ops.charm import CharmBase
+from ops.jujuversion import JujuVersion
 from ops.main import main
 
 
@@ -20,15 +21,28 @@ class SecretsCharmCharm(CharmBase):
         self.framework.observe(self.on.leader_elected, self._on_leader_elected)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
 
+    def has_secrets(self) -> bool:
+        juju_version = JujuVersion.from_environ()
+        # Because we're only using secrets in a peer relation we don't need to
+        # check if the other end of a relation also supports secrets...
+        return juju_version.has_secrets
+
     def _on_config_changed(self, _) -> None:
         peer_relation = self.model.get_relation("secrets-charm-peers")
-        secret_id = peer_relation.data[self.app]["secret-id"]
-        if secret_id:
-            secret = self.model.get_secret(id=secret_id)
-            secret_key = secret.get_content()["secret-key"]
-            logging.warning("secret-key set to %s", secret_key)
+        if self.has_secrets():
+            logging.warning("We support Juju secrets")
+            secret_id = peer_relation.data[self.app]["secret-id"]
+            if secret_id:
+                secret = self.model.get_secret(id=secret_id)
+                secret_key = secret.get_content()["secret-key"]
+                logging.warning("secret-key set to %s", secret_key)
+            else:
+                logging.error("Unable to get secret-key")
         else:
-            logging.error("Unable to get secret-key")
+            logging.warning("We don't support Juju secrets")
+            secret = peer_relation.data[self.app]["secret"]
+            logging.warning("secret set to %s", secret)
+
 
     def _on_leader_elected(self, _) -> None:
         peer_relation = self.model.get_relation("secrets-charm-peers")
@@ -37,8 +51,13 @@ class SecretsCharmCharm(CharmBase):
                 random.choice(string.ascii_letters + string.digits) for _ in range(32)
             )
             logging.warning("Secret value %s", secret_value)
-            secret = self.app.add_secret({"secret-key": secret_value})
-            peer_relation.data[self.app]["secret-id"] = secret.id
+            if self.has_secrets():
+                logging.warning("We support Juju secrets")
+                secret = self.app.add_secret({"secret-key": secret_value})
+                peer_relation.data[self.app]["secret-id"] = secret.id
+            else:
+                logging.warning("We don't support Juju secrets")
+                peer_relation.data[self.app]["secret"] = secret_value
 
 
 if __name__ == "__main__":  # pragma: nocover
